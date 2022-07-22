919a09c835674de4d39b22ce574304da3209378f
GitHub Action <action@github.com>
2022-07-22 07:40:03 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/toshibane/composeiconviewer/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.toshibane.composeiconviewer.services
+
+import com.github.toshibane.composeiconviewer.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/toshibane/composeiconviewer/services/MyProjectService.kt
+++ src/main/kotlin/com/github/toshibane/composeiconviewer/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.toshibane.composeiconviewer.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.toshibane.composeiconviewer.MyBundle
 
 class MyProjectService(project: Project) {
 
