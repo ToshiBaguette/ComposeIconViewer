997b0403fd502d8bf8f6b76c38565fa196c2db9c
Grégory LORY <glory@groupeseb.com>
2022-07-29 10:06:58 +0200

    Remove backward compatibility because jetbrains platform is bad

--- src/main/kotlin/fr/toshi/composeiconviewer/GutterComposeIconViewer.kt
+++ src/main/kotlin/fr/toshi/composeiconviewer/GutterComposeIconViewer.kt
@@ -1,10 +1,8 @@
 package fr.toshi.composeiconviewer
 
-import com.intellij.codeHighlighting.Pass
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerInfo
 import com.intellij.codeInsight.daemon.RelatedItemLineMarkerProvider
 import com.intellij.navigation.GotoRelatedItem
-import com.intellij.openapi.application.ApplicationInfo
 import com.intellij.openapi.editor.markup.GutterIconRenderer
 import com.intellij.openapi.util.NotNullFactory
 import com.intellij.psi.PsiElement
@@ -13,7 +11,6 @@ import javax.swing.Icon
 
 class GutterComposeIconViewer: RelatedItemLineMarkerProvider() {
     private val regexGetIcons = Regex("(Icons)\\.(.+)\\.([^,)]*)")
-    private val ideaVersion = ApplicationInfo.getInstance().build.baselineVersion
 
     override fun collectNavigationMarkers(
         element: PsiElement,
@@ -36,28 +33,15 @@ class GutterComposeIconViewer: RelatedItemLineMarkerProvider() {
 
     private fun getResult(element: PsiElement, icon: Icon): RelatedItemLineMarkerInfo<PsiElement> {
         /* Backward compatibility with older versions of IDEA */
-        return if (ideaVersion < 202) {
-            object : RelatedItemLineMarkerInfo<PsiElement>(
-                element,
-                element.textRange,
-                icon,
-                Pass.UPDATE_ALL,
-                null,
-                null,
-                GutterIconRenderer.Alignment.CENTER,
-                emptyList()
-            ) { }
-        } else {
-            object : RelatedItemLineMarkerInfo<PsiElement>(
-                element,
-                element.textRange,
-                icon,
-                null,
-                null,
-                GutterIconRenderer.Alignment.CENTER,
-                NotNullFactory<List<GotoRelatedItem>> { emptyList() }
-            ) { }
-        }
+        return object : RelatedItemLineMarkerInfo<PsiElement>(
+            element,
+            element.textRange,
+            icon,
+            null,
+            null,
+            GutterIconRenderer.Alignment.CENTER,
+            NotNullFactory<List<GotoRelatedItem>> { emptyList() }
+        ) { }
     }
 
     private fun getIconFromString(icon: String): Icon {
